becabf4413fa9ab2ec39f006814cdd2c3c441018
m-ops-181 <qhmiri@mail.de>
2026-05-31 16:54:51 +0200

    Router umstrukturiert und gekapselt

--- Software_Todo_umstrukturiert/model/todos_model.py
+++ Software_Todo_umstrukturiert/model/todos_model.py
@@ -1,10 +1,11 @@
 # pyright: reportUnknownMemberType=false
 from model.erzeuge_todo_model import ErzeugeTodoModel
+
 class TodosModel:
     def __init__(self):
         self._todos:list[ErzeugeTodoModel]=[]
     
-    def add_todo(self,todo):
+    def add_todo(self,todo:ErzeugeTodoModel):
         self._todos.append(todo)
 
     def get_todos(self)->list[ErzeugeTodoModel]:
