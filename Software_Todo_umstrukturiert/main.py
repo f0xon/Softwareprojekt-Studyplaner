d993754155d62ae5c0ff8c61e7bf975bb9596757
Selina <herbse01@thu.de>
2026-06-05 15:51:41 +0200

    Scrollen möglich

--- Software_Todo_umstrukturiert/main.py
+++ Software_Todo_umstrukturiert/main.py
@@ -7,6 +7,7 @@ os.chdir(os.path.dirname(__file__))
 
 def main(page: ft.Page):
     Router(page)
+    page.scroll = ft.ScrollMode.AUTO
     page.go("/Todo")
 
 ft.run(main, view=ft.AppView.WEB_BROWSER, port=8080)
