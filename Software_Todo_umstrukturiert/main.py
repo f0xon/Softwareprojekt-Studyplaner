ed171ec5628a249127d7c54c808def8aa8c0158a
Selina <herbse01@thu.de>
2026-06-05 16:26:42 +0200

    Webseiten-Titel

--- Software_Todo_umstrukturiert/main.py
+++ Software_Todo_umstrukturiert/main.py
@@ -6,6 +6,7 @@ from router import Router
 os.chdir(os.path.dirname(__file__))
 
 def main(page: ft.Page):
+    page.title= "ToDo-App"
     Router(page)
     page.scroll = ft.ScrollMode.AUTO
     page.go("/Todo")
