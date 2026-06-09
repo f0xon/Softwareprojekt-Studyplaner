d7746667534c8829326f359c281aec61c538787f
m-ops-181 <qhmiri@mail.de>
2026-06-09 18:07:16 +0200

    repo

--- Software_Todo_umstrukturiert/main.py
+++ Software_Todo_umstrukturiert/main.py
@@ -2,8 +2,29 @@
 import os
 import flet as ft
 from router import Router
-
+from pymongo import MongoClient
+from pymongo.database import Database
+from typing import Any
+from repo import MongoPersonTodoRepo,InMemoryTodoRepo
 os.chdir(os.path.dirname(__file__))
+
+#Nutze DB optionen: 'MongoPersonTodoRepo', 'InMemoryTodoRepo'
+Welche_DB: str = 'InMemoryTodoRepo'
+
+if(Welche_DB== 'InMemoryTodoRepo'):
+    InMemoryTodoRepo()
+elif(Welche_DB=='MongoPersonTodoRepo'):
+    #Logindaten Für die MongoDB Datenbank:
+    DB_URL = "mongodb+srv://cluster0.9w2gjme.mongodb.net"
+    DB_USER = "soen_labor"
+    DB_PASSWORD = "6HQgiBWd7IDAXa6g"
+    DB_NAME = "soen_vorlesung"
+
+    db: Database[Any] = MongoClient(DB_URL, username=DB_USER, password=DB_PASSWORD).get_database(DB_NAME)
+    MongoPersonTodoRepo(db)
+
+
+
 
 def main(page: ft.Page):
     page.title= "ToDo-App"
