e294647849d9a8227727eb5478e6bf16f1dc6185
m-ops-181 <qhmiri@mail.de>
2026-06-09 16:20:35 +0200

    protocol

--- Software_Todo_umstrukturiert/repo.py
+++ Software_Todo_umstrukturiert/repo.py
@@ -1,4 +1,10 @@
 from typing import Protocol
+from model.general_model import Todo
 
 class TodoRepo(Protocol):
-    def speichere(todo:Todo)
+    def speichere(self,todo:Todo):
+        ...
+    def lade_alle(self):
+        ...
+    def lade_todo(self,name:str):
+        ...
