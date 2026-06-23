8f084d560a45e7a5cc208f16faadadb33a558633
Ferdinand <fah@gmx.com>
2026-06-23 16:33:32 +0200

    Ropo zeug

--- Software_Todo_umstrukturiert/repo.py
+++ Software_Todo_umstrukturiert/repo.py
@@ -51,36 +51,24 @@ class MongoTodoRepo(TodoRepo):
             _todos.append(todo_obj)
         return ToDoListModel(todos=_todos)
 
-    def finde_todo_mit_id(self, todo_id: int) -> ToDoModel | None: 
+    def finde_todo_mit_id(self, todo_id: int) -> ToDoModel: 
         todo = self.db.todos.find_one({"_id": todo_id}, projection={"_id": False})
         if todo is None:
-            return None
+            raise ValueError(f"ToDo mit der Id-{todo_id} existiert nicht")
         return ToDoModel(**todo)
 
     def erledige_todo(self, todo_id: int) -> None:
-        todo = self.db.todos.find_one({"_id": todo_id})
+        todo = self.finde_todo_mit_id(todo_id)
+        #if todo is not None:
         neuer_status = not todo["_erledigt"]
         todo.update_one({"_id": todo_id}, {"$set": {"_erledigt": neuer_status}})
 
-    def loesche_todo(self, todo: ToDoModel) -> None:
-        self._collection.delete_one({"_id": todo.id})
+    def loesche_todo(self, todo_id: int) -> None:
+        todo= self.finde_todo_mit_id(todo_id)
+        todo.delete_one({"_id": todo.id})
     
     def filtere_todos(self, kat: str, prio: str, status: str) -> list[ToDoModel]:
-        query = {}
-        # Kategorie
-        if kat != "alle":
-            query["category"] = kategorien_dict[kat]
-        # Priorität
-        if prio != "alle":
-            query["priority"] = prioritäten_dict[prio]
-        # Status
-        if status == "offen":
-            query["erledigt"] = False
-        elif status == "erledigt":
-            query["erledigt"] = True
-        docs = self._collection.find(query)
-        return [ToDoModel(**doc) for doc in docs]
-
+        
     def naechste_id(self) -> int:...
 
 class InMemoryTodoRepo(TodoRepo):
